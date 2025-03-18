f7aa54f2c4edd4b460cc6096c9c1dc54846b300f
AdamYTV <adamhammadeh_4@hotmail.com>
2025-03-18 17:03:26 -0300

    18/03/2025

--- Aula05.js
+++ Aula05.js
@@ -4,10 +4,16 @@ console.log("hello world")
 // let nome = prompt("qual o seu nome");
 // console.log(nome);
 
+console.log("while");
 let i = 0;
 while(i <10){
     console.log(i)
     // i++;
     // i = i + 1;
     i += 1;
+}
+
+console.log("for");
+for(let i=0; i < 10; i++){
+    console.log(i);
 }
---
+++ aula05ex1.js
@@ -0,0 +1,24 @@
+//console.log("exercicos 1")
+//for(let i = 1; i < 100; i+= 2){
+    //console.log(i);
+//}
+
+//console.log("exercicos 2")
+//for(let i = 5; i <= 500; i+= 5){
+  //  console.log(i);
+//}
+
+//console.log("exercicos 3")
+//let x = parseInt(prompt("digite um numero inteiro"))
+//for(let i = x; i >= 0; i--){
+    //console.log(i);
+//}
+
+console.log("exercicios 4")
+let x = perseInt(prompt("digite um numero inteiro"))
+
+let mult = 1
+for(let i = x; i > 0; i--)(
+    mult *= i;
+    console.log(i,mult);
+)

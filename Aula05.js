341618c8acefa9c7e33bcfe04ed317db9f364404
AdamYTV <adamhammadeh_4@hotmail.com>
2025-03-18 16:25:17 -0300

    aula05

---
+++ Aula05.js
@@ -0,0 +1,13 @@
+// window.alert("hello world")
+console.log("hello world")
+
+// let nome = prompt("qual o seu nome");
+// console.log(nome);
+
+let i = 0;
+while(i <10){
+    console.log(i)
+    // i++;
+    // i = i + 1;
+    i += 1;
+}

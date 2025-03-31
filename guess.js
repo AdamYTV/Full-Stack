120ffe4dc9773960cedc5ed98c372e630b92a0a9
AdamYTV <adamhammadeh_4@hotmail.com>
2025-03-31 20:31:02 -0300

    31/05/2025

--- guess.js
+++ guess.js
@@ -1,6 +1,24 @@
 let num_aleatorio = Math.floor(Math.random() * 100);
 console.log (num_aleatorio);
 
-function guess(){
-    let num_aleatorio
+function adivinha(){
+    let guess = parseInt(document.getElementById("palpite").value);
+    let mensagem = document.getElementById("mensagem");
+    let fundo = document.getElementById("palpite");
+    
+    if (guess < num_aleatorio) {
+        mensagem.textContent = "Tente um número maior";
+        fundo.style.setProperty("background-color", "red");
+    }
+    else if (guess > num_aleatorio) {
+        mensagem.textContent = "Tente um número menor";
+        fundo.style.setProperty("background-color", "red");
+    }
+    else {
+        mensagem.textContent = "voce acertou";
+        fundo.style.setProperty("background-color", "green");
+        
+    }
 }
+
+

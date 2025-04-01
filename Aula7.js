1eaf74c919b74541f4a8b82365381caa4da9959f
AdamYTV <adamhammadeh_4@hotmail.com>
2025-04-01 14:21:10 -0300

    Aula7

--- Aula7.js
+++ Aula7.js
@@ -9,3 +9,17 @@ ctx.strokeStyle = 'red';
 ctx.fillRect(10,10,50,50);
 ctx.strokeRect(10,10,50,50);
 ctx.closePath();
+
+// linhas
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'blue';
+ctx.strokeStyle = 'red';
+ctx.moveTo(200,150);
+ctx.lineTo(60,10);
+ctx.lineTo(60,250);
+ctx.lineTo(200,250);
+ctx.lineTo(200,150);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
